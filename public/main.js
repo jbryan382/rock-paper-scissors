bcfb20194510c73e253a9aa57ad3f2ec836e3635
Jordan Bryan <jbryan382@gmail.com>
2019-02-06 11:31:06 -0500

    Player selection created and displays option

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let playerSelection
+
+const playerSelectedRock = () => {
+  playerSelection = 'Rock'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
 }
 
+const playerSelectedPaper = () => {
+  playerSelection = 'Paper'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
+}
+const playerSelectedScissors = () => {
+  playerSelection = 'Scissors'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
+}
+
+const main = () => {}
+
 document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('#rockButton')
+  .addEventListener('click', playerSelectedRock)
+document
+  .querySelector('#paperButton')
+  .addEventListener('click', playerSelectedPaper)
+document
+  .querySelector('#scissorsButton')
+  .addEventListener('click', playerSelectedScissors)
